78e1372168ca07299b12bb18414532678e32a503
Flancy <flancyk.flancyk@gmail.com>
2019-08-13 16:49:24 +0300

    Дизай страницы (как это работает)

--- resources/views/pages/review.blade.php
+++ resources/views/pages/review.blade.php
@@ -1,34 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-@include('elements.nav')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <h1>Отзыв или пожелание о сайте</h1>
-            <form>
-                <div class="form-row">
-                    <div class="col-md-4 mb-3">
-                        <label>Имя</label>
-                        <input type="text" class="form-control" placeholder="Имя" required>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label>E-mail</label>
-                        <input type="email" class="form-control" placeholder="E-mail" required>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label>Телефон</label>
-                        <input type="tel" class="form-control" placeholder="Телефон" required>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col-md-12 mb-3">
-                        <label>Отзыв или пожелание</label>
-                        <textarea class="form-control" style="height: 300px"></textarea>
-                    </div>
-                </div>
-                <button class="btn btn-success" type="submit">Отправить отзыв</button>
-            </form>
+<div class="who">
+    @include('elements.nav')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+               <h1>Как это работает?</h1>
+               <img src="{{ url('img/utrack-who.png') }}" alt="" class="img-fluid">
+            </div>
         </div>
     </div>
 </div>
